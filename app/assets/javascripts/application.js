58cf6c1db62b712268d3680a92eb1284c6333711
‘Nikesh <=>
2015-11-10 17:28:09 +0545

    added js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,20 @@
-// jqeury
+//= require jquery
+//= require classie
+//= require showRight
+//= require slide
 
 $('.js-nav').on('click', function(){
   $('.js-nav-move').toggleClass('nav-open');
 });
 
+
+
+  $(document).ready(function () {
+        var bigSlideAPI = ($('.menu-link').bigSlide({
+            side: 'right',
+            menuWidth: "320px"
+        })).bigSlideAPI;
+        $('.menu-close').click(function() {
+          bigSlideAPI.view.toggleClose();
+        });
+   });
--- app/assets/javascripts/showRight.js
+++ app/assets/javascripts/showRight.js
@@ -1,5 +1,8 @@
-showRight.onclick = function() {
-  classie.toggle( this, 'active' );
-  classie.toggle( menuRight, 'cbp-spmenu-open' );
-  disableOther( 'showRight' );
-};
+var=menuRight = document.getElementById( 'cbp-spmenu-s2' ), showRight = document.getElementById( 'showRight' ),body = document.body;
+
+  showRight.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuRight, 'cbp-spmenu-open' );
+  };
+  
+
