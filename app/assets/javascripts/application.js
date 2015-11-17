96d083f84617a17f7748776abaab67ae0bee679e
‘Nikesh <=>
2015-11-17 17:29:54 +0545

    manage padding and margin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,18 +3,29 @@
 //= require showRight
 //= require slide
 
+
 $('.js-nav').on('click', function(){
   $('.js-nav-move').toggleClass('nav-open');
 });
 
 
 
-  $(document).ready(function () {
-        var bigSlideAPI = ($('.menu-link').bigSlide({
-            side: 'right',
-            menuWidth: "320px"
-        })).bigSlideAPI;
-        $('.menu-close').click(function() {
-          bigSlideAPI.view.toggleClose();
-        });
-   });
+$(document).ready(function () {
+      var bigSlideAPI = ($('.menu-link').bigSlide({
+          side: 'right',
+          menuWidth: "320px"
+      })).bigSlideAPI;
+      $('.menu-close').click(function() {
+        bigSlideAPI.view.toggleClose();
+      });
+  $(".arrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#what-we-do").offset().top
+    }, 2000);
+  });
+  $(".arrow-up").click(function() {
+    $('html, body').animate({
+        scrollTop: $("html, body").offset().top
+    }, 2000);
+  });
+});
