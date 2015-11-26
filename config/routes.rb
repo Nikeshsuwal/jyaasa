8366680d73b3e5fade0d3bb8b758444899b8067a
rohinikhusu <rohini@jyaasa.com>
2015-11-26 17:01:52 +0545

    added article pade

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+  resources :pages do
+    collection do
+      get 'article'
+    end
+  end
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
